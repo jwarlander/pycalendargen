059f4ba995a942304ca653c4d4d030c071334fb6
Johan Warlander <johan@snowflake.nu>
2012-12-14 17:26:47 +0100

    Setting up basic test script to verify generated PDFs with 'known good' output

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -u
+
+echo "Generating PDF files for 2012-01 to 2012-12"
+
+for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
+  ./PyCalendarGen.py 2012 $month tmp/2012-${month}.pdf > tmp/2012-${month}.log &&\
+  comparepdf -c a -v 1 tmp/2012-${month}.pdf testdata/2012-${month}.pdf
+  result=$?
+  if [ "$result" -eq 0 ]; then
+    echo -n "."
+  else
+    echo "* Error while verifying 2012-${month}.pdf"
+    exit $result
+  fi
+done
+echo
