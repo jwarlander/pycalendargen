16a71f8f098abb419a467bd82b3276dd09ecc5ce
Johan Warlander <johan@snowflake.nu>
2012-12-16 00:03:10 +0100

    The test.sh script should create temp dir if needed; also make git ignore it

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -u
 
+if [ ! -d tmp/ ]; then
+  echo "Creating temporary directory for test output"
+  mkdir tmp/
+fi
+
 echo "Generating PDF files for 2012-01 to 2012-12"
 
 for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
