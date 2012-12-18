1600b09f673a1837203f1effcca22eb585ce02ac
Johan Warlander <johan@snowflake.nu>
2012-12-18 13:41:53 +0100

    Use ImageMagick for PDF comparison instead of 'comparepdf'

--- test.sh
+++ test.sh
@@ -10,12 +10,15 @@ echo "Generating PDF files for 2012-01 to 2012-12"
 
 for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
   ./PyCalendarGen.py 2012 $month tmp/2012-${month}.pdf > tmp/2012-${month}.log &&\
-  comparepdf -c a -v 1 tmp/2012-${month}.pdf testdata/2012-${month}.pdf
+  compare -metric AE tmp/2012-${month}.pdf testdata/2012-${month}.pdf png:- \
+    > /dev/null 2> tmp/2012-${month}.log
   result=$?
   if [ "$result" -eq 0 ]; then
     echo -n "."
   else
-    echo "* Error while verifying 2012-${month}.pdf"
+    echo "F"
+    echo "* Files tmp/2012-${month}.pdf and testdata/2012-${month}.pdf don't match:"
+    cat tmp/2012-${month}.log
     exit $result
   fi
 done
